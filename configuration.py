51b0c813a5f89e586fcd5a3a525d43dcd136d181
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-05-31 15:54:02 -0500

    cambios al config para conectar con mysql

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 
 # Sqlite
-cadena_base_datos = 'sqlite:///final1bimaa22.db' 
+#cadena_base_datos = 'sqlite:///final1bimaa22.db' 
 
 # Mysql
-#cadena_base_datos = 'mysql+mysqlconnector://mateo:12345@localhost:3306/final1bimaa22'
+cadena_base_datos = 'mysql+mysqlconnector://mateo:12345@localhost:3306/final1bimaa22'
